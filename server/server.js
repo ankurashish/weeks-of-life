c743b58987a05d63fc6901811b71a10754acbe38
Ankur Ashish <ankurashishindia@gmail.com>
2025-06-15 17:36:18 +0530

    update pr files

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use("/api/auth", authRoutes);
 // Serve frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
